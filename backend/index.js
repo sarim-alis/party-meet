a8c74769bf83c96b17e10b8c1849283b83a7aa4c
sarim-alis <sarimslayerali786@gmail.com>
2026-01-02 05:31:20 +0500

    feat server setup

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// Imports.
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -5,24 +6,19 @@ import connectDB from "./utils/db.js";
 import eventRoutes from "./src/routes/event.route.js";
 import userRoutes from "./src/routes/user.route.js";
 
+// Middleware.
 dotenv.config();
 connectDB();
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
+// Routes.
 app.use("/api/users", userRoutes);
 app.use("/api/events", eventRoutes);
+app.get("/", (req, res) => { res.send("PartyMeet API running 🎉")});
 
-app.get("/", (req, res) => {
-  res.send("PartyMeet API running 🎉");
-});
-
+// Server.
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT} 🦊🌟❤️`));
